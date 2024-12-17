dab9592e7013d561d14cb6ea6a32c236aef38b63
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-17 20:44:05 -0300

    inicializando projeto

---
+++ src/main/java/com/lopeshenriquee/TabelaFipe/service/TabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package com.lopeshenriquee.TabelaFipe.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaFipeApplication.class, args);
+	}
+
+}
