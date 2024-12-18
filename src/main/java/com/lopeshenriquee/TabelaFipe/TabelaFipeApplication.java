c9d49aba5a7d236705db62c87b054d4b71f2667f
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-17 21:53:29 -0300

    implements CommandLinRunner

--- src/main/java/com/lopeshenriquee/TabelaFipe/TabelaFipeApplication.java
+++ src/main/java/com/lopeshenriquee/TabelaFipe/TabelaFipeApplication.java
@@ -1,13 +1,20 @@
 package com.lopeshenriquee.TabelaFipe;
 
+import com.lopeshenriquee.TabelaFipe.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TabelaFipeApplication {
+public class TabelaFipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabelaFipeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.showMenu();
+	}
 }
